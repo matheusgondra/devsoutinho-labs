cf432cebed82bae69ef31c1e3db3fe4db6c74aa6
Matheus de Gondra <matheusgondra951@gmail.com>
2023-01-19 15:05:06 -0300

    :lipstick: feat: estilizando o output

--- challenge-1/src/App.tsx
+++ challenge-1/src/App.tsx
@@ -6,21 +6,21 @@ export const App = () => {
     <div className="container">
       <h1 className="title">[Previewer]</h1>
       <h2 className="subtitle">border-radius:</h2>
-      <div>
-        <span>border-radius: 50px 0 0 0;</span>
-        <div>
-          <div>
+      <div className="previewer_container">
+        <span className="previewer_output">border-radius: 50px 0 0 0;</span>
+        
+          <div className="previewer_input-container">
             <InputBorderRadius text="Top Left" />
             <InputBorderRadius text="Top Right" />
           </div>
 
-          <div></div>
+          <div className="previewer_view"></div>
 
-          <div>
+          <div className="previewer_input-container">
             <InputBorderRadius text="Bottom Left" />
             <InputBorderRadius text="Bottom Right" />
           </div>
-        </div>
+        
       </div>
     </div>
   );
