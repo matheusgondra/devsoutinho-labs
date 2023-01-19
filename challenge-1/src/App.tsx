818c16e59377b420bab3c817bd585b2c736eba41
Matheus de Gondra <matheusgondra951@gmail.com>
2023-01-19 15:13:14 -0300

    :lipstick: feat: Finalizando estilização mobile

--- challenge-1/src/App.tsx
+++ challenge-1/src/App.tsx
@@ -8,19 +8,18 @@ export const App = () => {
       <h2 className="subtitle">border-radius:</h2>
       <div className="previewer_container">
         <span className="previewer_output">border-radius: 50px 0 0 0;</span>
-        
-          <div className="previewer_input-container">
-            <InputBorderRadius text="Top Left" />
-            <InputBorderRadius text="Top Right" />
-          </div>
 
-          <div className="previewer_view"></div>
+        <div className="previewer_input-container">
+          <InputBorderRadius text="Top Left" />
+          <InputBorderRadius text="Top Right" />
+        </div>
 
-          <div className="previewer_input-container">
-            <InputBorderRadius text="Bottom Left" />
-            <InputBorderRadius text="Bottom Right" />
-          </div>
-        
+        <div className="previewer_view"></div>
+
+        <div className="previewer_input-container">
+          <InputBorderRadius text="Bottom Left" />
+          <InputBorderRadius text="Bottom Right" />
+        </div>
       </div>
     </div>
   );
