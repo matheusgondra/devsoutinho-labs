90de0ef9e1be7948c26cb712d36c8252350e35c8
Matheus de Gondra <matheusgondra951@gmail.com>
2023-01-20 12:51:53 -0300

    :sparkles: feat: funcionalidade de border radius preview

--- challenge-1/src/App.tsx
+++ challenge-1/src/App.tsx
@@ -1,7 +1,39 @@
 import { InputBorderRadius } from "./components/InputBorderRadius";
 import "./App.css";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 export const App = () => {
+  const [topLeft, setTopLeft] = useState("");
+  const [topRight, setTopRight] = useState("");
+  const [bottomLeft, setBottomLeft] = useState("");
+  const [bottomRight, setBottomRight] = useState("");
+
+  const previewRef = useRef<HTMLDivElement>(null);
+
+  const handleBorderRadius = (e: ChangeEvent<HTMLInputElement>, direction: string) => {
+    const value = e.target.value;
+
+    switch (direction) {
+      case "topLeft":
+        return setTopLeft(value);
+      case "topRight":
+        return setTopRight(value);
+      case "bottomLeft":
+        return setBottomLeft(value);
+      case "bottomRight":
+        return setBottomRight(value);
+    }
+  }
+
+  useEffect(() => {
+    if(previewRef.current){
+      previewRef.current.style.borderTopLeftRadius = `${topLeft}px`;
+      previewRef.current.style.borderTopRightRadius = `${topRight}px`;
+      previewRef.current.style.borderBottomLeftRadius = `${bottomLeft}px`;
+      previewRef.current.style.borderBottomRightRadius = `${bottomRight}px`;
+    }
+  }, [topLeft, topRight, bottomLeft, bottomRight]);
+
   return (
     <div className="container">
       <h1 className="title">[Previewer]</h1>
@@ -10,15 +42,15 @@ export const App = () => {
         <span className="previewer_output">border-radius: 50px 0 0 0;</span>
 
         <div className="previewer_input-container">
-          <InputBorderRadius text="Top Left" />
-          <InputBorderRadius text="Top Right" />
+          <InputBorderRadius handleChange={handleBorderRadius} direction="topLeft" text="Top Left" />
+          <InputBorderRadius handleChange={handleBorderRadius} direction="topRight" text="Top Right" />
         </div>
 
-        <div className="previewer_view"></div>
+        <div ref={previewRef} className="previewer_view"></div>
 
         <div className="previewer_input-container">
-          <InputBorderRadius text="Bottom Left" />
-          <InputBorderRadius text="Bottom Right" />
+          <InputBorderRadius handleChange={handleBorderRadius} direction="bottomLeft" text="Bottom Left" />
+          <InputBorderRadius handleChange={handleBorderRadius} direction="bottomRight" text="Bottom Right" />
         </div>
       </div>
     </div>
--- challenge-1/src/components/InputBorderRadius/index.tsx
+++ challenge-1/src/components/InputBorderRadius/index.tsx
@@ -1,14 +1,34 @@
+import { ChangeEvent, useState } from "react";
 import "./styles.css";
 
 interface InputBorderRadiusProps {
     text: string;
+    handleChange: (e: ChangeEvent<HTMLInputElement>, direction: string) => void;
+    direction: string;
 }
 
-export const InputBorderRadius = ({ text }: InputBorderRadiusProps) => {
+export const InputBorderRadius = ({ text, handleChange, direction }: InputBorderRadiusProps) => {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if (!isNaN(Number(value))) {
+            setInputValue(value);
+        }
+    }
+
     return (
         <label className="border-radius_label">
             {text}
-            <input className="border-radius_input" type="text" />
+            <input
+                onChange={(e) => {
+                    handleInputValue(e);
+                    handleChange(e, direction);
+                }}
+                value={inputValue}
+                className="border-radius_input"
+                type="text"
+            />
         </label>
     );
 }
